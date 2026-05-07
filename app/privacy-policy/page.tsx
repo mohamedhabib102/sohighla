3d38855c8840e51c296a467e5197d8dfd5d36edd
Mohamed Habib <byaalkhy43@gmail.com>
2026-05-07 19:10:09 +0300

    feat: add static pages for terms and conditions and privacy policy

---
+++ app/privacy-policy/page.tsx
@@ -0,0 +1,14 @@
+
+
+
+const Page = () => {
+    return (
+        <section>
+            <h1>Terms & Conditions</h1>
+        </section>
+    )
+};
+
+
+
+export default Page;
---
+++ app/terms/page.tsx
@@ -0,0 +1,12 @@
+
+
+
+const Page = () => {
+    return (
+        <section>
+            <h1>Terms and Conditions</h1>
+        </section>
+    )
+};
+
+export default Page;
